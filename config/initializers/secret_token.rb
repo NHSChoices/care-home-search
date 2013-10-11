3afac9ad1a782b4cdfac29431796abc3ab80f70d
Russell <russell@russelldunphy.com>
2013-10-11 10:15:57 +0100

    :boom: First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CareHomeSearch::Application.config.secret_key_base = '309f181d54bfff47a97392d36535207a39ed2e0ea800bdb3f1dc54b38396dc101da26a60c481aa8cbf00abbd9d2b526e76d9560a90ae465ea7992cda2f4f423e'
